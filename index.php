a34592aa88ce4e08c978789acd75fc45496a18ba
PhilipJames <pjkelnhofer@gmail.com>
2015-09-22 20:19:32 -0400

    Fixed typos

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
             <div class="form-group">
               <input type="checkbox" name="addNumber" <?=$numCheck?>>
               <label for="addNumber">Random number
-              <select name="numbers" id="addNumbers">
+              <select name="numbers" id="addNumber">
                 <option value="1" <?php echo $numbers == '1' ? 'selected="selected"' : ''; ?>>1</option>
                 <option value="2" <?php echo $numbers == '2' ? 'selected="selected"' : ''; ?>>2</option>
                 <option value="3" <?php echo $numbers == '3' ? 'selected="selected"' : ''; ?>>3</option>
@@ -83,7 +83,7 @@
             <div class="form-group">
               <input type="checkbox" name="addChar" <?=$charCheck?>>
               <label for="addChar">Random characters
-              <select name="chars">
+              <select name="chars" is="addChar">
                 <option value="1" <?php echo $chars == '1' ? 'selected="selected"' : ''; ?>>1</option>
                 <option value="2" <?php echo $chars == '2' ? 'selected="selected"' : ''; ?>>2</option>
                 <option value="3" <?php echo $chars == '3' ? 'selected="selected"' : ''; ?>>3</option>
